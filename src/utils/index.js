d6ff2a624a2e18973b0d88c3b3629b3b6a078323
palzuncoff <palzuncoff@yahoo.com>
2018-12-22 14:52:41 +0200

    Create tomorrow handle

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,47 +6,57 @@ function scheduleHoursRange(day) {
         return 'not working'
     }
     const [{from, to}] = day;
-    const arrFrom = from.split(':')
-    const arrTo = to.split(':')
+    const arrFrom = from.split(':');
+    const arrTo = to.split(':');
     return `${moment({ hour:arrFrom[0], minute:arrFrom[1] }).format('LT')} - ${moment({ hour:arrTo[0], minute:arrTo[1] }).format('LT')}`
 }
 
 function inRange({ from, to }) {
-    const arrFrom = from.split(':')
-    const arrTo = to.split(':')
+    const arrFrom = from.split(':');
+    const arrTo = to.split(':');
     return moment().isBetween(moment({ hour:arrFrom[0], minute:arrFrom[1] }), moment({ hour:arrTo[0], minute:arrTo[1] }));
 }
 
 function getFirstWorkDay(ISchedule) {
     const entries = Object.entries(ISchedule);
-    const dayName = DAYS_READABLE[entries[0][0]]
-    const time = entries[0][1][0].from.split(':')
+    const dayName = DAYS_READABLE[entries[0][0]];
+    const time = entries[0][1][0].from.split(':');
     return `${dayName} at ${moment({ hour:time[0], minute:time[1] }).format('LT')}`
 }
 
+function getTomorrowWorkHours(ISchedule) {
+    const day = ISchedule[DAYS[moment().day()]];
+    const time = day[0].from.split(':');
+    return moment({ hour: time[0], minute: time[1] }).format('LT');
+}
+
+function getNextThisWeekWorkDay(ISchedule, restDays) {
+
+}
+
 export function getHumanReadableSchedule(ISchedule) {
-    const entries = Object.entries(ISchedule)
+    const entries = Object.entries(ISchedule);
     if (entries.length < 1) return null;
     return DAYS.reduce((acc, dayId, index) => {
-        const day = ISchedule[dayId]
+        const day = ISchedule[dayId];
         if (index === 0) {
-            acc.push(`${dayId.toUpperCase()} : ${scheduleHoursRange(day)}`)
+            acc.push(`${dayId.toUpperCase()} : ${scheduleHoursRange(day)}`);
             return acc;
         }
-        const prevDay = ISchedule[DAYS[index - 1]]
+        const prevDay = ISchedule[DAYS[index - 1]];
         if (scheduleHoursRange(day) === scheduleHoursRange(prevDay)) {
-            const lastResult = acc[acc.length - 1]
+            const lastResult = acc[acc.length - 1];
             const indexA = lastResult.indexOf(':');
-            const indexB = lastResult.indexOf('-')
+            const indexB = lastResult.indexOf('-');
             let newString;
             if (indexB > 0 && indexB < indexA) {
-                const subString = lastResult.slice(indexB + 1, 3)
+                const subString = lastResult.slice(indexB + 1, 3);
                 newString = lastResult.replace(subString, dayId.toUpperCase())
             } else {
                 newString = lastResult.replace(' :', `-${dayId.toUpperCase()} :`)
             }
-            acc.pop()
-            acc.push(newString)
+            acc.pop();
+            acc.push(newString);
             return acc;
         }
         acc.push(`${dayId.toUpperCase()} : ${scheduleHoursRange(day)}`);
@@ -62,23 +72,25 @@ export function isInWorkingHours(ISchedule) {
 export function getHumanReadableNextWorkingHours(ISchedule) {
     if (!isInWorkingHours(ISchedule)) {
         const restDays = () => {
-            const dayIndex = moment().day() - 1
+            const dayIndex = moment().day() - 1;
             return DAYS.slice(dayIndex)
         };
-        const result = restDays().reduce((acc, day, index, days) => {
-            if (ISchedule[day]) {
-                acc.push(ISchedule[day])
+        return restDays().reduce((acc, day, index, days) => {
+            if (ISchedule[DAYS[moment().day() - 1]]) {
+                console.log(ISchedule[day]);
                 return acc;
             }
-            const nextWorkDay = days.find(day => !!ISchedule[day])
-            if (nextWorkDay) {
+            if (ISchedule[days[1]]) {
+                acc = `Tomorrow at ${getTomorrowWorkHours(ISchedule)}`;
                 return acc;
             }
-            acc = `Next week on ${getFirstWorkDay(ISchedule)}`
+            if (false) {
+                acc = `On at ${getNextThisWeekWorkDay(ISchedule)}`;
+                return acc;
+            }
+            acc = `Next week on ${getFirstWorkDay(ISchedule)}`;
             return acc;
         }, '');
-
-        return result;
     }
 
     return '';
