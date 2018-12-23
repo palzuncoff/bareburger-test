dacd37084ed3205917a968bbb53aacdefbedc1c9
palzuncoff <palzuncoff@yahoo.com>
2018-12-23 14:45:57 +0200

    load Stores from db

--- src/components/stores-list.js
+++ src/components/stores-list.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import Store from './store';
-
-const stores = [{id: 1}, {id: 2}];
+import api from '../utils/api';
 
 class StoresList extends Component {
+    state = {
+        stores: [],
+        error: null,
+    }
+    componentDidMount() {
+        return api.fetchAllStores()
+            .catch(e => this.setState({ error: e }))
+            .then(res => {
+                const resArray = Object.entries(res)
+                return this.setState({ stores: resArray })
+            })
+    }
     renderStores = () => {
-        return stores.map(store => <Store key={store.id}/>)
+        return this.state.stores.map(store => <Store
+            key={store[0]}
+            name={store[1].name}
+            schedule={store[1].schedule}
+            />
+        )
     };
     render() {
         return (
