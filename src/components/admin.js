27a35645bf4cce70cc20745794077a677fde960e
palzuncoff <palzuncoff@yahoo.com>
2018-12-26 13:04:10 +0200

    Create manage Store component

--- src/components/admin.js
+++ src/components/admin.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
 import { DAYS } from '../constants';
+import api from "../utils/api";
+import ManageStore from './manage-store';
+import Store from "./stores-list";
 
 const hours = Array(24).fill(null).map((_, i) => i < 10 ? `0${1}` : `${i}`);
 const minutes = Array(60).fill(null).map((_, i) => i < 10 ? `0${1}` : `${i}`);
 
 class Admin extends Component {
     state = {
-        name: '',
-        schedule: {}
-    };
-    handleName = e => {
-        this.setState({ name: e.target.value })
+        stores: [],
+        error: null,
+    }
+    componentDidMount() {
+        return api.fetchAllStores()
+            .catch(e => this.setState({ error: e }))
+            .then(res => {
+                const resArray = Object.entries(res)
+                return this.setState({ stores: resArray })
+            })
+    }
+
+    renderStoreList = () => {
+        return this.state.stores.map(store => <ManageStore
+            key={store[0]}
+            id={store[0]}
+            name={store[1].name}
+            schedule={store[1].schedule}
+            timeZone={store[1].timeZone}
+        />)
     }
     render() {
         return (
             <div>
-                <input type="text" value={this.state.name} onChange={this.handleName}/>
+                <ManageStore />
                 <ul>
-                    {DAYS.map(day => (
-                        <li key={day}>
-                            <span>{day}</span>
-                        </li>
-                    ))}
+                    {this.renderStoreList()}
                 </ul>
             </div>
         )
---
+++ src/components/manage-store.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ManageStore extends Component {
+    state = {
+        name: this.props.name,
+        schedule: this.props.schedule,
+        timeZone: this.props.timeZone,
+    };
+
+    handleName = e => {
+        this.setState({ name: e.target.value })
+    };
+    render() {
+        const { name, schedule, timeZone } = this.state;
+        return (
+            <li>
+                <input onChange={this.handleName} value={name}/>
+                <button>Save</button>
+                <button>Delete</button>
+            </li>
+        );
+    }
+}
+
+ManageStore.defaultProps = {
+    id: 'djhkgsldhgvihdvgadshf',
+    name: 'Store',
+    schedule: {
+        mon: [{ from: '10:00', to: '23:00' }],
+        tue: [{ from: '10:00', to: '23:00' }],
+        wed: [{ from: '09:00', to: '13:30' }, { from: '14:00', to: '22:00' }],
+        thu: [{ from: '11:00', to: '23:00' }],
+        fri: [{ from: '12:00', to: '23:00' }],
+    },
+    timeZone: 'Asia/Shanghai',
+};
+
+ManageStore.propTypes = {
+    id: PropTypes.string,
+    name: PropTypes.string,
+    schedule: PropTypes.shape({}),
+    timeZone: PropTypes.string,
+};
+
+export default ManageStore;
